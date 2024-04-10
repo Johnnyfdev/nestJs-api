6dd021fe25a872370f9a2954c699e2fba160d9bd
johnnyfdev <johnnyfreirejh@hotmail.com>
2024-04-10 18:17:41 -0300

    Added prisma and create category

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Delete, Get, Injectable, Post, Put } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoriesService {
+
+constructor(private prisma:PrismaService) {}
+
+  @Post()
   create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+    return this.prisma.category.create({
+      data : createCategoryDto,
+    });
   }
 
+  @Get()
   findAll() {
-    return `This action returns all categories`;
+    return this.prisma.category.findMany();
   }
 
+  @Get(':id')
   findOne(id: number) {
-    return `This action returns a #${id} category`;
+   return this.prisma.category.findUnique({
+     where: {id}
+   });
   }
 
+  @Put(':id')
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+    return this.prisma.category.update({
+      where: {id},
+      data : updateCategoryDto,
+    });
   }
 
+  @Delete(':id')
   remove(id: number) {
-    return `This action removes a #${id} category`;
+    return this.prisma.category.delete({
+      where: {id}
+    });
   }
 }
